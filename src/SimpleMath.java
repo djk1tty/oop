2e16f3d8f1ae44d4d5c712203a78624cc7d4445b
djk1tty <Daniil.Kolokoltsev@mail.ru>
2024-01-26 20:12:15 +0300

    Initial commit

---
+++ src/SimpleMath.java
@@ -0,0 +1,10 @@
+public class SimpleMath {
+
+    public static double getSum(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double getMul(double num1, double num2) {
+        return num1 * num2;
+    }
+}
